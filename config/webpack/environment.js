89d3d1a353be00820449d802e02662e1ace42f1c
abdul-hanan-1 <a.hanan.rana@gmail.com>
2021-12-29 18:37:48 +0500

    added theme

---
+++ config/webpack/environment.js
@@ -0,0 +1,17 @@
+// config/webpack/environment.js
+const { environment } = require('@rails/webpacker')
+const webpack = require("webpack");
+
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.append(
+    "Provide",
+    new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        $: "jquery-hoverIntent",
+        $: "jquery-waypoints",
+        Popper: ["popper.js", "default"]
+    })
+);
+
+module.exports = environment
